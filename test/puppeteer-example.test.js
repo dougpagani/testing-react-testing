74ec84f61f1b72191b8bc1dcc630f8907c8a0cc5
dougpagani <redstonefreedom@gmail.com>
2020-06-20 19:31:34 -0400

    better dev experience from reattaching

--- test/puppeteer-example.test.js
+++ test/puppeteer-example.test.js
@@ -1,4 +1,8 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+
+// dont proliferate new instances
+// const browserWSEndpoint = 'a browser websocket endpoint to connect to';
+// const browser = await puppeteer.connect({browserWSEndpoint});
 
 const isDebugging = () => {
   const debugging_mode = {
@@ -19,7 +23,14 @@ const isDebugging = () => {
 
 describe('on page load', () => {
   it('h1 loads correctly', async() => {
-    let browser = await puppeteer.launch(isDebugging())
+    let freshInstance = false
+    let browser 
+    if (freshInstance) {
+      browser = await browser.newPage()
+    } else {
+      const browserWSEndpoint = 'ws://127.0.0.1:63538/devtools/browser/bd02cf08-4a53-4d0e-a7a6-7523b88df9c3'
+      browser = await puppeteer.connect({browserWSEndpoint});
+    }
     let page = await browser.newPage()
     
     page.emulate({
@@ -30,8 +41,15 @@ describe('on page load', () => {
       userAgent: ''
     })
     await page.goto('http://localhost:3000/');
+    await page.evaluate(() => { console.log('ayo') })
+    await console.log(page.browser())
+    // DONTCOMMIT dump
+    // const fs = require('fs'); fs.writeFileSync('dump.json', JSON.stringify(JSON.decycle(page.browser()))); console.log('NOTE: object written to dump.json')
+    // need to grab ws link from this dump
+    console.log('hello')
     // const html = await page.$eval('.App-title', e => e.innerHTML);
     // expect(html).toBe('Welcome to React');
     // browser.close();
+    browser.disconnect()
   }, 160000); // test timeout of 16s
 })
